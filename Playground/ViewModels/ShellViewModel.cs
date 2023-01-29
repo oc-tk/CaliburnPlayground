e1bbce14e75b9a39f1dc4414a978f338df8c57d8
Tomek Kaczmarzyk <tspamm3r@gmail.com>
2023-01-29 23:34:37 +0100

    29.01.2023 playin

--- Playground/ViewModels/ShellViewModel.cs
+++ Playground/ViewModels/ShellViewModel.cs
@@ -7,6 +7,17 @@ namespace Playground.ViewModels
 {
     public class ShellViewModel: Screen
     {
-
+        string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return this._firstName;
+            }
+            set
+            {
+                this._firstName = value;
+            }
+        }
     }
 }
